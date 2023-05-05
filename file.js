9b6c7710c75009d29f6ae7a65052c3d001dce5b4
SHjalilo <aaaabdjalil@gmail.com>
2023-05-05 16:53:19 +0100

    done!

--- file.js
+++ file.js
@@ -1,8 +1,33 @@
 // https://jsonplaceholder.typicode.com/posts?userId=1
 
-function getPostWithId(){
+function getUserName(){
   let requset = new XMLHttpRequest();
-  requset.open("GET","https://jsonplaceholder.typicode.com/posts?userId=2");
+  requset.open("GET","https://jsonplaceholder.typicode.com/users");
+  requset.responseType = "json";
+  requset.send();
+  requset.onload = function(){
+    if(requset.status >= 200 && requset.status < 300){
+      let users = requset.response;
+      document.getElementById("left").innerHTML ="";
+      let index = 1;
+      for(user of users){
+        let content = `<div onclick="getPostWithId(${index});" class="left"><h1>${user.name}</h1><h4>${user.email}</h4></div>`;
+        document.getElementById("left").innerHTML +=content;
+        index++;
+      }
+      console.log("status code is : "+requset.status);
+    }else {
+      alert("error !");
+    }
+  }
+}
+getUserName();
+
+//
+
+function getPostWithId(index){
+  let requset = new XMLHttpRequest();
+  requset.open("GET","https://jsonplaceholder.typicode.com/posts?userId="+index);
   requset.responseType = "json";
   requset.send();
   requset.onload = function(){
@@ -20,26 +45,5 @@ function getPostWithId(){
   }
 }
 
-getPostWithId();
 
 //
-function getUserName(){
-  let requset = new XMLHttpRequest();
-  requset.open("GET","https://jsonplaceholder.typicode.com/users");
-  requset.responseType = "json";
-  requset.send();
-  requset.onload = function(){
-    if(requset.status >= 200 && requset.status < 300){
-      let users = requset.response;
-      document.getElementById("left").innerHTML ="";
-      for(user of users){
-        let content = `<div class="left"><h1>${user.name}</h1><h4>${user.email}</h4></div>`;
-        document.getElementById("left").innerHTML +=content;
-      }
-      console.log("status code is : "+requset.status);
-    }else {
-      alert("error !");
-    }
-  }
-}
-getUserName();
